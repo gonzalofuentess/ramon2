cf77e01e942bd1e14d584d51ba800fc26b5edc8e
gonzalo <gonzalo@DESKTOP-18A7HM4>
2019-04-07 22:07:04 -0300

    arreglos de código

--- config/index.php
+++ config/index.php
@@ -7,95 +7,96 @@ include '../static/sesion.php';
     <head>
         <?php include '../static/head.html'; ?>
 
+        <script type="text/javascript" src="../assets/google/loader.js"></script>
+    
 
-    </head>
-    <body>
-        <?php include '../static/encabezado.html'; ?>
 
-        <div id="body-container">
-            <div id="body-content">
-                <?php include '../static/menu.html'; ?>
+</head>
+<body>
+    <?php include '../static/encabezado.html'; ?>
 
-                <section class="nav nav-page">
-                    <div class="container">
-                        <div class="row">
-                            <div class="span7">
-                                <header class="page-header">                             
-                                    <h3>Configuración <br>
-                                        <small>Radio Monitoreo FM</small>
-                                    </h3>
-                                </header>
-                            </div>
-                            <div class="page-nav-options">
-                                <div class="span9">
-                                    <ul class="nav nav-pills">
-                                        <li>
-                                            <a href="../main/"><i class="icon-home icon-large"></i></a>
-                                        </li>
-                                    </ul>
-                                    <ul class="nav nav-tabs">
+    <div id="body-container">
+        <div id="body-content">
+            <?php include '../static/menu.html'; ?>
 
-                                        <?php
-                                        if (isset($_GET["menu"])) {
-                                            switch ($_GET["menu"]) {
-                                                case "correo":
-                                                    ?>
-                                                    <li><a href="../config/"><i class="icon-home"></i>Radio</a></li>
-                                                    <li class="active"><a><i class="icon-envelope-alt"></i>Correo</a></li>                                                   
-                                                    <?php
-                                                    break;
-                                            }
-                                        } else {
-                                            ?>
-                                            <li class = "active"><a><i class="icon-home"></i>Radio </a></li>
-                                            <li><a href = "../config/index.php?menu=correo"><i class="icon-envelope-alt"></i>Correo</a></li>
-                                        <?php }
+            <section class="nav nav-page">
+                <div class="container">
+                    <div class="row">
+                        <div class="span7">
+                            <header class="page-header">                             
+                                <h3>Configuración <br>
+                                    <small>Radio Monitoreo FM</small>
+                                </h3>
+                            </header>
+                        </div>
+                        <div class="page-nav-options">
+                            <div class="span9">
+                                <ul class="nav nav-pills">
+                                    <li>
+                                        <a href="../main/"><i class="icon-home icon-large"></i></a>
+                                    </li>
+                                </ul>
+                                <ul class="nav nav-tabs">
+
+                                    <?php
+                                    if (isset($_GET["menu"])) {
+                                        switch ($_GET["menu"]) {
+                                            case "correo":
+                                                ?>
+                                                <li><a href="../config/"><i class="icon-home"></i>Radio</a></li>
+                                                <li class="active"><a><i class="icon-envelope-alt"></i>Correo</a></li>                                                   
+                                                <?php
+                                                break;
+                                        }
+                                    } else {
                                         ?>
+                                        <li class = "active"><a><i class="icon-home"></i>Radio </a></li>
+                                        <li><a href = "../config/index.php?menu=correo"><i class="icon-envelope-alt"></i>Correo</a></li>
+                                    <?php }
+                                    ?>
 
-                                    </ul>
-                                </div>
+                                </ul>
                             </div>
                         </div>
                     </div>
-                </section>
-
-
-
-                <?php
-                if (isset($_GET["menu"])) {
-                    
-                    switch ($_GET["menu"]){
-                        case  "correo":
-                            
-                            include './correo.html';
-                            break;                        
-                        
-                    }
-                    
-                } else {
-
-                    include './main.html';
-                }
-                ?>
-
-
-            </div>
-        </div>
-        <div id="spinner" class="spinner" style="display:none;">
-            Loading&hellip;
-        </div>
-
-        <footer class="application-footer">
-            <div class="container">
-                <p>Ramon</p>
-                <div class="disclaimer">
-                    <p>Radio Monitoreo FM</p>
-                    <p>Copyright © 2018-2019</p>
                 </div>
-            </div>
-        </footer>
+            </section>
 
-        <?php include '../static/script.html'; ?>
-    </body>
+
+
+            <?php
+            if (isset($_GET["menu"])) {
+
+                switch ($_GET["menu"]) {
+                    case "correo":
+
+                        include './correo.html';
+                        break;
+                }
+            } else {
+
+                include './main.html';
+            }
+            ?>
+
+
+        </div>
+    </div>
+    <div id="spinner" class="spinner" style="display:none;">
+        Loading&hellip;
+    </div>
+
+    <footer class="application-footer">
+        <div class="container">
+            <p>Ramon</p>
+            <div class="disclaimer">
+                <p>Radio Monitoreo FM</p>
+                <p>Copyright © 2018-2019</p>
+            </div>
+        </div>
+    </footer>
+
+<?php include '../static/script.html'; ?>
+</body>
 
 </html>
